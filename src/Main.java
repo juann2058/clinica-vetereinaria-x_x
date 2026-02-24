48e91f1eb7c05dcaea13767f2a61416e3bed9e28
JAND <juanandresninopirachicam@gmail.com>
2026-02-23 21:51:00 -0500

    menu

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,17 @@ void main() {
 
 Scanner teclado = new Scanner(System.in);
 
-do while{
-
-    }
+int opcion;
+do {
+    System.out.println(" Bienvenido");
+    System.out.println("1. mostrar informacion mascota");
+    System.out.println("2. alimentar mascota");
+    System.out.println("3. vacunar");
+    System.out.println("4. cumpleaños");
+    System.out.println("5. mostrar informacion final mascota");
+    opcion = teclado.nextInt();
+    switch (opcion)
+}
 
 
 
